d1b0949fa358f27812003235459d71570df0656c
yao <yao@yao-PC>
2013-11-10 16:34:53 +0800

    update project

--- armorer-engine/src/org/armorer/engine/util/CommonUtil.java
+++ armorer-engine/src/org/armorer/engine/util/CommonUtil.java
@@ -4,10 +4,9 @@ import org.apache.log4j.Logger;
 
 public class CommonUtil {
 
-
-	public static void showInfo(String className,String info){
-	    Logger logger = Logger.getLogger (className);
+    public static void showInfo(String className, String info) {
+        Logger logger = Logger.getLogger(className);
         logger.info(info);
         System.out.println(info);
-	}
+    }
 }
